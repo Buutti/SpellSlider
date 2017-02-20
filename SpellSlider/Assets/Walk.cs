05d028e26c0eaa24a7471340f2304ff36c1f9671
Essi Viippola <essi.viippola@live.fi>
2017-02-20 19:29:19 +0200

    Creating "game view"
    Initial implementation of the game view

---
+++ SpellSlider/Assets/Walk.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour {
+
+    public GameObject wizard;
+    public int speed;
+    Vector3 startingPosition = new Vector3();
+
+	// Use this for initialization
+    // Initializing monster's starting position
+	void Start () {
+        startingPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+    // Moving the monster
+	void Update () {
+        transform.position -= new Vector3(speed, 0, 0)*Time.deltaTime;
+
+        // if monster reaches the wizard, it is transported back
+        if(transform.position.x <= wizard.transform.position.x + 15)
+        {
+            transform.position = startingPosition;
+        }
+    }
+}
