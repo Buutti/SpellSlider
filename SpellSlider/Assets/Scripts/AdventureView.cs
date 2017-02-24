b097e5bf2bb48fd56100c7465a2b6d716c8127fa
Toni Kuosmanen <toni.t.kuosmanen@gmail.com>
2017-02-24 22:17:32 +0200

    Fiddled around with transforms

--- SpellSlider/Assets/Scripts/AdventureView.cs
+++ SpellSlider/Assets/Scripts/AdventureView.cs
@@ -6,7 +6,12 @@ public class AdventureView : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        gameObject.transform.position = new Vector3
+        {
+            x = 0,
+            y = Camera.main.orthographicSize * 2,
+            z = gameObject.transform.position.z
+        };
 	}
 	
 	// Update is called once per frame
