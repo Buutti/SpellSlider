134d303c1e71f3b15548f55b7288b1ffd60b925d
Essi Viippola <essi.viippola@live.fi>
2017-02-24 15:05:46 +0200

    Added music player and script for the menu button

---
+++ SpellSlider/Assets/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    public static MusicPlayer Instance;
+
+	// Use this for initialization
+	void Start () {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+
+	}
+	
+}
--- SpellSlider/Assets/Scripts/MenuManager.cs
+++ SpellSlider/Assets/Scripts/MenuManager.cs
@@ -1,10 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class MenuManager : MonoBehaviour {
     
     public  void LoadLevel(string sceneName) {
         SceneManager.LoadScene(sceneName);
     }
+
+    public void ExitLevel()
+    {
+        try
+        {
+            Level level = FindObjectOfType<Level>();
+
+            if (level != null)
+            {
+                Destroy(level.gameObject);
+            }
+        
+            SceneManager.LoadScene("LevelSelect");
+        }
+        catch (Exception e)
+        {
+            throw;
+        }
+    }
+        
 }
