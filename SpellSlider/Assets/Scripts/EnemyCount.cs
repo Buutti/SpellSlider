421cb177dd80f60c4322a513ab74a02c7c374d48
Toni Kuosmanen <toni.t.kuosmanen@gmail.com>
2017-03-20 22:44:28 +0200

    Changes to LevelManager. Levels are now handled as PreFabs.  Added extension methods for shuffling and swapping list elements.
    - Added Extensions.cs. Added extension methods for shuffling and
    swapping list elements.
    - Changes to enemies on level 1 and 2
    - Added Randomized bool property to Level. If true, enemy queue is
    shuffled before enemies are drawn. Randomization is handled in
    AdventureView.Start() before adding enemies to EnemyQueue.

---
+++ SpellSlider/Assets/Scripts/EnemyCount.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class EnemyCount : System.Object {
+    public EnemyManager.EnemyType EnemyType;
+    public int Count;
+}
