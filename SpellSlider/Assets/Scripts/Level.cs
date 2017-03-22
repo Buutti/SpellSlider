58a39eeb3ae5d2c1dd0f9df3ee6ecc4f05cab79f
Essi Viippola <essi.viippola@live.fi>
2017-03-22 16:38:30 +0200

    Revert "Changes to LevelManager. Levels are now handled as PreFabs.  Added extension methods for shuffling and swapping list elements."
    This reverts commit 421cb177dd80f60c4322a513ab74a02c7c374d48.

--- SpellSlider/Assets/Scripts/Level.cs
+++ SpellSlider/Assets/Scripts/Level.cs
@@ -8,11 +8,6 @@ public class Level : MonoBehaviour {
     /// <summary>
     /// Queue containing all the enemies in the level
     /// </summary>
-    public List<EnemyCount> EnemyCountList;
-
-    /// <summary>
-    /// Determines if enemy queue is randomized after creation
-    /// </summary>
-    public bool Randomized;
+    public List<EnemyManager.EnemyType> EnemyTypeList;
 
 }
--- SpellSlider/Assets/Scripts/LevelManager.cs
+++ SpellSlider/Assets/Scripts/LevelManager.cs
@@ -6,27 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    public Level LEVEL_1_1;
-    public Level LEVEL_1_2;
-    public Level EMPTY;
 
     public enum Levels
     {
         LEVEL_1_1,
         LEVEL_1_2,
-        LEVEL_1_3,
-        LEVEL_1_4,
-        LEVEL_1_5,
         EMPTY
-    }
-
-    private Dictionary<Levels, Level> levelDictionary;
-
-    private void Start()
-    {
-        levelDictionary = new Dictionary<Levels, Level>();
-        levelDictionary.Add(Levels.LEVEL_1_1, LEVEL_1_1);
-        levelDictionary.Add(Levels.LEVEL_1_2, LEVEL_1_2);
     }
 
     private static Levels GetLevelEnum(string levelName)
@@ -42,20 +27,11 @@ public class LevelManager : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// Load level and move to game scene.
-    /// </summary>
-    /// <param name="levelName">Name of the level to be loaded</param>
     public void LoadLevel(string levelName)
     {
-        Level level = Instantiate(GetLevel(GetLevelEnum(levelName)));
+        GameObject level = PopulateLevel(GetLevelEnum(levelName));
         DontDestroyOnLoad(level);
         SceneManager.LoadScene("Game");
-    }
-
-    public Level GetLevel ( Levels level) {
-        
-        return levelDictionary[level];
     }
 
     private static GameObject PopulateLevel(Levels levelEnum)
@@ -64,33 +40,33 @@ public class LevelManager : MonoBehaviour
         level.name = "Level";
         level.AddComponent<Level>();
 
-        //switch (levelEnum)
-        //{
-        //    case Levels.LEVEL_1_1:
-        //        level.GetComponent<Level>().EnemyTypeList = new List<EnemyManager.EnemyType>
-        //        {
-        //            EnemyManager.EnemyType.RockMonster,
-        //            EnemyManager.EnemyType.RockMonster,
-        //            EnemyManager.EnemyType.RockMonster,
-        //            EnemyManager.EnemyType.RockMonster,
-        //            EnemyManager.EnemyType.RockMonster,
-        //            EnemyManager.EnemyType.RockMonster
-        //        };
-        //        break;
-        //    case Levels.LEVEL_1_2:
-        //        level.GetComponent<Level>().EnemyTypeList = new List<EnemyManager.EnemyType>
-        //        {
-        //            EnemyManager.EnemyType.RockMonster,
-        //            EnemyManager.EnemyType.IceMonster,
-        //            EnemyManager.EnemyType.FireMonster,
-        //            EnemyManager.EnemyType.SandMonster,
-        //        };
-        //        break;
+        switch (levelEnum)
+        {
+            case Levels.LEVEL_1_1:
+                level.GetComponent<Level>().EnemyTypeList = new List<EnemyManager.EnemyType>
+                {
+                    EnemyManager.EnemyType.RockMonster,
+                    EnemyManager.EnemyType.RockMonster,
+                    EnemyManager.EnemyType.RockMonster,
+                    EnemyManager.EnemyType.RockMonster,
+                    EnemyManager.EnemyType.RockMonster,
+                    EnemyManager.EnemyType.RockMonster
+                };
+                break;
+            case Levels.LEVEL_1_2:
+                level.GetComponent<Level>().EnemyTypeList = new List<EnemyManager.EnemyType>
+                {
+                    EnemyManager.EnemyType.RockMonster,
+                    EnemyManager.EnemyType.IceMonster,
+                    EnemyManager.EnemyType.FireMonster,
+                    EnemyManager.EnemyType.SandMonster,
+                };
+                break;
 
-        //    default:
-        //        level.GetComponent<Level>().EnemyTypeList = new List<EnemyManager.EnemyType>();
-        //        break;
-        //}
+            default:
+                level.GetComponent<Level>().EnemyTypeList = new List<EnemyManager.EnemyType>();
+                break;
+        }
 
         return level;
     }
